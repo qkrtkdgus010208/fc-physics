6080b892b8305e97439b288cd7b33baf4befd6d8
qkrtkdgus010208 <psh010208@naver.com>
2025-05-19 20:04:49 +0900

    Ch03-08
    struct 타입 관련 코드 주석 추가

--- Assets/E13/Scripts/E13HingeJointController.cs
+++ Assets/E13/Scripts/E13HingeJointController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class E13HingeJointController : MonoBehaviour
 {
     [SerializeField] private HingeJoint[] hinges;
-    private JointMotor[] motors;
+    private JointMotor[] motors; // struct 타입이므로 수정을 위해 복사 → 수정 → 다시 대입 과정이 꼭 필요
     private Rigidbody rb;
     private float moveSpeed = 5f;
 
@@ -17,8 +17,9 @@ public class E13HingeJointController : MonoBehaviour
         for (int i=0; i < hinges.Length; i++)
         {
             hinges[i].useMotor = true;
-            motors[i] = hinges[i].motor;
-            motors[i].targetVelocity = -1000f;
+            motors[i] = hinges[i].motor; // motor를 복사 (값 복사)
+            motors[i].targetVelocity = -1000f; // 복사본을 수정
+            hinges[i].motor = motors[i]; // 수정된 복사본을 다시 원본에 할당 (여기 필수!)
         }
     }
 
@@ -28,8 +29,8 @@ public class E13HingeJointController : MonoBehaviour
         {
             for (int i=0; i < hinges.Length; i++)
             {
-                motors[i].force = 500;
-                hinges[i].motor = motors[i];
+                motors[i].force = 500; // 복사본을 수정
+                hinges[i].motor = motors[i]; // 수정된 복사본을 다시 원본에 할당 (여기 필수!)
             }
         }
 
@@ -37,8 +38,8 @@ public class E13HingeJointController : MonoBehaviour
         {
             for (int i=0; i<hinges.Length; i++)
             {
-                motors[i].force = 0;
-                hinges[i].motor = motors[i];
+                motors[i].force = 0; // 복사본을 수정
+                hinges[i].motor = motors[i]; // 수정된 복사본을 다시 원본에 할당 (여기 필수!)
             }
         }
     }
